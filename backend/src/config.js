8b9166f135d50729b89cee5ee508d134a9b8e2fb
Raghav Agrawal <raghav.github69@gmail.com>
2025-09-11 04:50:32 +0530

    added parser functionalities

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  WORKER_URL: process.env.WORKER_URL || 'https://2eabe2296e5c.ngrok-free.app',
+  WORKER_URL: process.env.WORKER_URL || 'http://0.0.0.0:8081',
   DB_PATH: process.env.DB_PATH || './data/sfe.db',
 };
