67a3e49c1ebdbe90c9fb7c1f0383371481a0be55
Raghav Agrawal <raghav.github69@gmail.com>
2025-09-11 00:20:31 +0530

    fixing integration

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  WORKER_URL: process.env.WORKER_URL || 'https://5f7237491f5f.ngrok-free.app',
+  WORKER_URL: process.env.WORKER_URL || 'https://2eabe2296e5c.ngrok-free.app',
   DB_PATH: process.env.DB_PATH || './data/sfe.db',
 };
